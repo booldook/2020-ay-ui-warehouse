b2a7fdf37e610422758fc064f5ca4d4e27c13435
booldook <booldook@gmail.com>
2020-02-17 17:05:48 +0900

    완성

--- js/prd.js
+++ js/prd.js
@@ -2,14 +2,14 @@
 var $prd = $(".prd");
 var last = $prd.length - 1; //10 (11개 그림의 마지막 Index)
 
-
 /* 초기화 */
-var now = 0;
-init(now);
-function init(st) {
+var now = last;
+var dir = "P"; // P => target: 0, L => -50%
+init();
+function init() {
 	$(".prd").remove();
 	$(".prd-wrap").css({"left": "-25%"});
-	for(var i=st, k; i<st+6; i++) {
+	for(var i=now, k; i<now+6; i++) {
 		if(i > last) k = i - last - 1;
 		else k = i;
 		$(".prd-wrap").append($prd[k]);
@@ -17,11 +17,30 @@ function init(st) {
 }
 
 function ani() {
-	$(".prd-wrap").stop().animate({"left": "-50%"}, 500, function(){
-		if(now < last) now++;
-		else now = 0;
-		init(now);
+	if(dir == "P") target = "0";
+	else target = "-50%";
+	$(".prd-wrap").stop().animate({"left": target}, 500, function(){
+		if(dir == "P") {
+			if(now == 0) now = last;
+			else now--;
+		}
+		else {
+			if(now < last) now++;
+			else now = 0;
+		}
+		init();
 	});
 }
 
-interval = setInterval(ani, 1000);
+$(".bt-prev").click(onPrev);
+$(".bt-next").click(onNext);
+
+function onPrev() {
+	dir = "P";
+	ani();
+}
+
+function onNext() {
+	dir = "L";
+	ani();
+}
