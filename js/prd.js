0f91698cb01d3634684bbf3692b2e1f81b5b92da
booldook <booldook@gmail.com>
2020-02-18 15:41:53 +0900

    prd.html

--- js/prd.js
+++ js/prd.js
@@ -1,11 +1,34 @@
+/* 통신에러 체크 */
+function onErr(err) {
+	console.log(err);
+}
+
+
+
 /* .prd를 변수에 넣기 */
-var prd = $(".prd");
-var last = prd.length - 1; //10 (11개 그림의 마지막 Index)
+var prd = [], last, now, dir, html;
+
+/* 통신으로 데이터 가져오기 */
+$.get('http://192.168.1.88:3000/warehouse').then(onRes).catch(onErr);
+
+function onRes(res) {
+	// console.log(res);
+	// console.log(res.prd);
+	// console.log(res.prd[0]);
+	for(var i in res.prd) {
+		html  = '<div class="prd">';
+		html += '<img src="./img/'+res.prd[i].src+'" class="img">';
+		html += '</div>';
+		prd[i] = $(html);
+	}
+	last = prd.length - 1;
+	now = last;
+	init();
+}
+
+
 
 /* 초기화 */
-var now = last;
-var dir = "P"; // P => target: 0, L => -50%
-init();
 function init() {
 	$(".prd-wrap").empty();
 	$(".prd-wrap").css({"left": "-25%"});
